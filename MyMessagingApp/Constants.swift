880c4cf907cd99620be264117222de0cde73f1ad
Sahil Agashe <sahil@scrambleapps.gmail.com>
2023-11-27 08:54:22 +0530

    Added User.swift and Constants.swift files

---
+++ MyMessagingApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MyMessagingApp
+//
+//  Created by SAHIL AMRUT AGASHE on 27/11/23.
+//
+
+import Foundation
+
+public let kCURRENTUSER = "currentUser"
+
---
+++ MyMessagingApp/User.swift
@@ -0,0 +1,45 @@
+//
+//  User.swift
+//  MyMessagingApp
+//
+//  Created by SAHIL AMRUT AGASHE on 27/11/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct User:Codable, Equatable {
+    var id = ""
+    var username: String
+    var email: String
+    var pushId = ""
+    var avatarLink = ""
+    var status: String
+    
+    static var currentId: String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    static var currentUser: User? {
+        
+        if Auth.auth().currentUser != nil {
+            if let dictionary = UserDefaults.standard.data(forKey: kCURRENTUSER) {
+                let decoder = JSONDecoder()
+                do {
+                    let userObject = try decoder.decode(User.self, from: dictionary)
+                    return userObject
+                } catch {
+                    print("DEBUG: Error while decoding User from dictionary is \(error.localizedDescription)")
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+}
